1ac3a809708d521cb4e61e06b294eda88fa54b81
guiguiguiguigui <sophiaphia@live.com>
2016-02-20 21:51:32 -0500

    home screen

--- src/WHack_Cat_App.c
+++ src/WHack_Cat_App.c
@@ -1,60 +1,104 @@
+
 #include <pebble.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-static Window *window;
-static TextLayer *text_layer;
 
-static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Select");
-}
+#include "modules/health.h"
 
-static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Up");
-}
+#include "windows/main_window.h"
 
-static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Down");
-}
 
-static void click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
-}
 
-static void window_load(Window *window) {
+static Window *s_main_window;
+static TextLayer *s_title_layer;
+static GBitmap *s_steps_icon_bitmap, *s_sleep_icon_bitmap, *s_water_icon_bitmap;
+static BitmapLayer *s_steps_icon_bitmap_layer, *s_sleep_icon_bitmap_layer, *s_water_icon_bitmap_layer;
+static const int icon_size = 30;
+static const int side_margin = 10;
+static const int bottom_margin = 5;
+
+static void main_window_load(Window *window) {
+  // Get information about the Window
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  text_layer = text_layer_create(GRect(0, 72, bounds.size.w, 20));
-  text_layer_set_text(text_layer, "Press a button");
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+  // Create the TextLayer with specific bounds
+  s_title_layer = text_layer_create(
+      GRect(0, 0, bounds.size.w, 50));
+
+  // Improve the layout to be more like a watchface
+  text_layer_set_background_color(s_title_layer, GColorClear);
+  text_layer_set_text_color(s_title_layer, GColorBlack);
+  text_layer_set_text(s_title_layer, "Catnap");
+  text_layer_set_font(s_title_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(s_title_layer, GTextAlignmentCenter);
+
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(window_layer, text_layer_get_layer(s_title_layer));
+  
+  // .png pictures (bitmap): will need to be specified as bitmap resource type in appinfo.json !!!
+  // https://developer.pebble.com/guides/pebble-apps/resources/image-resources/
+  // also need 40px icons!!!
+  
+  // bitmap layers (icons)
+  s_steps_icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_STEPS_IMAGE);
+  s_steps_icon_bitmap_layer = bitmap_layer_create(GRect(side_margin, bounds.size.h-icon_size-bottom_margin, icon_size, icon_size));
+  bitmap_layer_set_bitmap(s_steps_icon_bitmap_layer, s_steps_icon_bitmap);
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_steps_icon_bitmap_layer));
+  
+  s_sleep_icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_SLEEP_IMAGE);
+  s_sleep_icon_bitmap_layer = bitmap_layer_create(GRect((bounds.size.w/2)-(icon_size/2), bounds.size.h-icon_size-bottom_margin, icon_size, icon_size));
+  bitmap_layer_set_bitmap(s_sleep_icon_bitmap_layer, s_sleep_icon_bitmap);
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_sleep_icon_bitmap_layer));
+  
+  s_water_icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_WATER_IMAGE);
+  s_water_icon_bitmap_layer = bitmap_layer_create(GRect(bounds.size.w-icon_size-side_margin, bounds.size.h-icon_size-bottom_margin, icon_size, icon_size));
+  bitmap_layer_set_bitmap(s_water_icon_bitmap_layer, s_water_icon_bitmap);
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_water_icon_bitmap_layer));
+
+}	  
+  		  
+static void select_click_handler(ClickRecognizerRef recognizer, void *context) { 
+  health_init();
+  main_window_push();
+  main_window_update_ui();
 }
 
-static void window_unload(Window *window) {
-  text_layer_destroy(text_layer);
+
+static void main_window_unload(Window *window) {
+  // Destroy TextLayer
+  text_layer_destroy(s_title_layer);
 }
 
-static void init(void) {
-  window = window_create();
-  window_set_click_config_provider(window, click_config_provider);
-  window_set_window_handlers(window, (WindowHandlers) {
-    .load = window_load,
-    .unload = window_unload,
+  
+static void click_config_provider(void *context) {
+  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
+}
+
+static void init() {
+  // Create main Window element and assign to pointer
+  s_main_window = window_create();
+
+  // Set handlers to manage the elements inside the Window
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload
   });
-  const bool animated = true;
-  window_stack_push(window, animated);
+
+  // Show the Window on the watch, with animated=true
+  window_stack_push(s_main_window, true);
+  window_set_click_config_provider(s_main_window, click_config_provider);
 }
 
-static void deinit(void) {
-  window_destroy(window);
+static void deinit() {
+  // Destroy Window
+  window_destroy(s_main_window);
 }
 
 int main(void) {
   init();
-
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
-
   app_event_loop();
   deinit();
 }
